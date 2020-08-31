5dc0f336d0f04802acd9be4cc03e0bb1f7c76b6c
Daniel MacMonagle <44430071+DanielMacMonagle@users.noreply.github.com>
2020-08-31 12:57:24 +0100

    Add files via upload

--- MyGui.java
+++ MyGui.java
@@ -1,26 +1,47 @@
 import java.awt.FlowLayout;
-
+import java.awt.*;  
+import java.awt.event.*;  
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 public class MyGui extends JFrame implements ActionListener {
 
 
-    public static void main(String args[]) {
 
-    
+
+    public MyGui() {
 
     JFrame addingJButton = new JFrame("Button Question");
     JButton button1 = new JButton("Click me for message");
-    button1.setBounds(80,30,300,200);
+    
+    button1.setBounds(300,200,300,200);
     addingJButton.add(button1);
 
     addingJButton.getContentPane().setLayout(new FlowLayout());
     addingJButton.setSize(300,200);
-
+    addingJButton.setLocationRelativeTo(null);
 
     addingJButton.setVisible(true);
+
+    button1.addActionListener(this);
+
     
+
+
+    }
+
+    public static void main(String args[]) {
+
+    new MyGui();
+}
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        JOptionPane.showMessageDialog(null, "Button has been clicked");
     }
 }
