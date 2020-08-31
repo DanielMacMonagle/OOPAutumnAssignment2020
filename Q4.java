ee309ee9bb3270a0fdf64b3c4eabea3e6ffe3812
Daniel MacMonagle <44430071+DanielMacMonagle@users.noreply.github.com>
2020-08-31 13:24:54 +0100

    Add files via upload

--- Q4.java
+++ Q4.java
@@ -5,13 +5,22 @@ public class Q4 {
     public static void main(String args[]){
 
         int memSize;
+        int outofRange;
 
         
         try{
             memSize = Integer.parseInt(JOptionPane.showInputDialog("Enter Age"));
+
+            if(memSize > 100 || memSize < 0){
+                throw new IllegalArgumentException
+                ("Out of range value, value must be between 0 - 100");
+            }
+
         }catch(NumberFormatException e){
             JOptionPane.showMessageDialog(null, "Please enter a valid age");
         }
+
+
 
 
 
